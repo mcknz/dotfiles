d512e52a03fd515bd8c784a27a607c77847472f3
Jeff McKenzie <mail@mcknz.com>
2012-06-08 12:02:46 -0400

    added np++ and home aliases

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -6,3 +6,5 @@ alias lsa='ls -A'                              # all but . and ..
 alias hgrep='history | grep'
 alias c3='cd /c/DevStuff/!Projects/github/cover-three-client'
 alias sv2='cd /c/sv2-git'
+alias np='/c/DevStuff/Apps/Notepad++/notepad++.exe'
+alias hm='cd ~'
